958f15267df043e9d54ddbf896098fb963d0c9a6
Rebakure <kevinrebakure@gmail.com>
2026-01-05 17:31:39 +0200

    chore: create users table

---
+++ src/main/resources/db/migration/V1__create_users_table.sql
@@ -0,0 +1,13 @@
+create table users
+(
+    id       bigint generated always as identity
+        constraint users_pk
+            primary key,
+    name     varchar(255) not null,
+    email    varchar(255) not null
+        constraint unique_email
+            unique,
+    password varchar(255) not null,
+    role     varchar(50)  not null
+);
+
