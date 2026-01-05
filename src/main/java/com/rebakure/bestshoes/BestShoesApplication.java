06650008e88a2a1f39b064395a02b7f0bf6f2ae3
Rebakure <kevinrebakure@gmail.com>
2026-01-05 16:48:20 +0200

    init

---
+++ src/main/java/com/rebakure/bestshoes/BestShoesApplication.java
@@ -0,0 +1,12 @@
+package com.rebakure.bestshoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestShoesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestShoesApplication.class, args);
+    }
+}
