abb88f7dc976cc0bdee50961bd6f4c0ec32526a1
Rebakure <kevinrebakure@gmail.com>
2026-01-13 14:52:11 +0200

    feat: get current user

--- src/main/java/com/rebakure/bestshoes/controllers/AuthController.java
+++ src/main/java/com/rebakure/bestshoes/controllers/AuthController.java
@@ -4,6 +4,8 @@ import com.rebakure.bestshoes.dtos.JwtResponse;
 import com.rebakure.bestshoes.dtos.LoginRequest;
 import com.rebakure.bestshoes.dtos.RegisterUserRequest;
 import com.rebakure.bestshoes.dtos.UserDto;
+import com.rebakure.bestshoes.mappers.UserMapper;
+import com.rebakure.bestshoes.repositories.UserRepository;
 import com.rebakure.bestshoes.services.AuthService;
 import com.rebakure.bestshoes.services.JwtService;
 import jakarta.validation.Valid;
@@ -13,12 +15,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 @AllArgsConstructor
 public class AuthController {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
     private AuthService authService;
     private final AuthenticationManager authenticationManager;
     private final JwtService  jwtService;
@@ -43,6 +48,21 @@ public class AuthController {
         var token = jwtService.generateToken(request.getEmail());
 
         return ResponseEntity.ok(new JwtResponse(token));
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> me() {
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        var email = (String) authentication.getPrincipal();
+        var user = userRepository.findUserByEmail(email);
+
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        var userDto = userMapper.entityToDto(user);
+
+        return ResponseEntity.ok(userDto);
     }
 
     @ExceptionHandler(BadCredentialsException.class)
